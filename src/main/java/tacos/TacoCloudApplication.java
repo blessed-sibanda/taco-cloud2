b35043b6eddd5121c7948aebb9c08b7065bc602b
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-10-22 12:36:43 +0200

    database

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,7 +1,12 @@
 package tacos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tacos.data.IngredientRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +15,22 @@ public class TacoCloudApplication {
         SpringApplication.run(TacoCloudApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+            var ingredients = List.of(
+                    new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                    new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                    new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                    new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                    new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                    new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                    new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                    new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                    new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                    new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+            );
+            repo.saveAll(ingredients);
+        };
+    }
 }
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,6 @@
 package tacos.web;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 import javax.validation.Valid;
 
-@Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+    private final OrderRepository orderRepo;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
@@ -29,7 +36,7 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
     }
