e1573a1a7d0cb66902d1f67803b5605461259701
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-10-22 10:33:57 +0200

    annotate the domain as entities

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,12 +1,22 @@
 package tacos;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
-    private final String id;
-    private final String name;
-    private final Type type;
+    @Id
+    private String id;
+    private String name;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
