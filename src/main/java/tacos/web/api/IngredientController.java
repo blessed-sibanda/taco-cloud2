2b8e976c474055b170ad514bbead4b5c9273a529
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-10-24 04:21:52 +0200

    ingredient api

---
+++ src/main/java/tacos/web/api/IngredientController.java
@@ -0,0 +1,36 @@
+package tacos.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import tacos.Ingredient;
+import tacos.data.IngredientRepository;
+
+@RestController
+@RequestMapping(path = "/api/ingredients", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
+public class IngredientController {
+    private final IngredientRepository repo;
+
+    @Autowired
+    public IngredientController(IngredientRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping
+    public Iterable<Ingredient> allIngredients() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+        return repo.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId) {
+        repo.deleteById(ingredientId);
+    }
+}
